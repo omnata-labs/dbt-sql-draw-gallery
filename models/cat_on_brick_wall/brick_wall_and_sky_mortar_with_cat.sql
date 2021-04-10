56a74ed3d3adb3a4858ed93f2f19b4abb11a60d0
James Weakley <jameswillisweakley@gmail.com>
2021-04-10 19:16:44 +1000

    Fix rectangle macro

--- models/cat_on_brick_wall/brick_wall_and_sky_mortar_with_cat.sql
+++ models/cat_on_brick_wall/brick_wall_and_sky_mortar_with_cat.sql
@@ -8,10 +8,10 @@ select x,y,
         when {{ circle_filled([45,138],4) }} then '#C1B04F' -- eye 1
         when {{ circle_filled([55,138],4) }} then '#C1B04F' -- eye 1
         
-        when {{ rectangle(top_left=[45,100], bottom_right=[50,120])}} then '#D2691E' -- leg 1
-        when {{ rectangle(top_left=[55,100], bottom_right=[60,120])}} then '#D2691E' -- leg 2
-        when {{ rectangle(top_left=[80,100], bottom_right=[85,120])}} then '#D2691E' -- leg 3
-        when {{ rectangle(top_left=[90,100], bottom_right=[95,120])}} then '#D2691E' -- leg 4
+        when {{ rectangle(bottom_left=[45,100], top_right=[50,120])}} then '#D2691E' -- leg 1
+        when {{ rectangle(bottom_left=[55,100], top_right=[60,120])}} then '#D2691E' -- leg 2
+        when {{ rectangle(bottom_left=[80,100], top_right=[85,120])}} then '#D2691E' -- leg 3
+        when {{ rectangle(bottom_left=[90,100], top_right=[95,120])}} then '#D2691E' -- leg 4
         when {{ circle_filled([50,135],15) }} then '#D2691E' -- head
         when {{ oval_filled(center=[60,150], diameter=20, ratio_x=0.5,ratio_y=1) }} then '#D2691E' -- ear 1
         when {{ oval_filled(center=[40,150], diameter=20, ratio_x=0.5,ratio_y=1) }} then '#D2691E' -- ear 2
