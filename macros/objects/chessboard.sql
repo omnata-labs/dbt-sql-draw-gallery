b8055db81d23ee5284556d653f92e3a09a6422f6
James Weakley <jameswillisweakley@gmail.com>
2021-04-13 08:30:22 +1000

    Enclose comments in Jinja comment blocks, so they don't spill into the output

--- macros/objects/chessboard.sql
+++ macros/objects/chessboard.sql
@@ -1,4 +1,4 @@
--- This is just a special type of tessellating rectangle, with equal width sides and black and white colours
+{# --This is just a special type of tessellating rectangle, with equal width sides and black and white colours #}
 {% macro chessboard(width=10) %}
 {{ tessellating_rectangle(width=width,height=width,odd_colour='#ffffff',even_colour='#000000')}}
 {% endmacro %}
--- macros/utilities/colour_between.sql
+++ macros/utilities/colour_between.sql
@@ -1,6 +1,6 @@
 {% macro colour_between(first_value_column='colour1',second_value_column='colour2') %}
--- split out red, green, blue with ints in 0-255 range
--- we want to pick the integer value between the two. E.g. (0,0,0) and (100,200,255) becomes (50,100,128)
+{#-- split out red, green, blue with ints in 0-255 range
+  -- we want to pick the integer value between the two. E.g. (0,0,0) and (100,200,255) becomes (50,100,128) #}
 '#' ||
 lpad(to_hex(({{hex_to_rgb(second_value_column,1)}} + {{hex_to_rgb(first_value_column,1)}}) / 2),2,'0') ||
 lpad(to_hex(({{hex_to_rgb(second_value_column,2)}} + {{hex_to_rgb(first_value_column,2)}}) / 2),2,'0') ||
