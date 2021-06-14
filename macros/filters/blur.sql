5e8a614a7dd80ea08922e69503caadec8f2b4d8d
James Weakley <jameswillisweakley@gmail.com>
2021-06-14 20:24:24 +1000

    Not quite a fix

--- macros/filters/blur.sql
+++ macros/filters/blur.sql
@@ -1,3 +1,3 @@
 {% macro blur() %}
-whennnn LEAD(colour) OVER ( PARTITION BY x ORDER BY x,y) is not null then {{ colour_between(first_value_column='colour',second_value_column='LEAD(colour) OVER ( PARTITION BY x ORDER BY x,y)') }}
+whennn LEAD(colour) OVER ( PARTITION BY x ORDER BY x,y) is not null then {{ colour_between(first_value_column='colour',second_value_column='LEAD(colour) OVER ( PARTITION BY x ORDER BY x,y)') }}
 {% endmacro %}
