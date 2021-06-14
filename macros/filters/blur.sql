3b0f8a79b5b4816f6be368f68dc6535d3d3cb6d4
James Weakley <jameswillisweakley@gmail.com>
2021-06-14 20:28:09 +1000

    Restore correct syntax

--- macros/filters/blur.sql
+++ macros/filters/blur.sql
@@ -1,3 +1,3 @@
 {% macro blur() %}
-whennn LEAD(colour) OVER ( PARTITION BY x ORDER BY x,y) is not null then {{ colour_between(first_value_column='colour',second_value_column='LEAD(colour) OVER ( PARTITION BY x ORDER BY x,y)') }}
+when LEAD(colour) OVER ( PARTITION BY x ORDER BY x,y) is not null then {{ colour_between(first_value_column='colour',second_value_column='LEAD(colour) OVER ( PARTITION BY x ORDER BY x,y)') }}
 {% endmacro %}
