e251d455c1f7760e53ac226baff5b5e83695d0d2
James Weakley <jameswillisweakley@gmail.com>
2021-08-15 18:07:49 +1000

    fix rainbow

--- macros/objects/colour_rainbow.sql
+++ macros/objects/colour_rainbow.sql
@@ -6,5 +6,6 @@ case
     when y >= {{ center[1] }} and POWER(x-{{ center[0] }},2) + POWER(y-{{ center[1] }},2) between POWER({{ (height)-(4*stripe_height) }},2) and POWER({{ (height)-(3*stripe_height) }},2) then '#4DE94C' -- green
     when y >= {{ center[1] }} and POWER(x-{{ center[0] }},2) + POWER(y-{{ center[1] }},2) between POWER({{ (height)-(5*stripe_height) }},2) and POWER({{ (height)-(4*stripe_height) }},2) then '#3783FF' -- blue
     when y >= {{ center[1] }} and POWER(x-{{ center[0] }},2) + POWER(y-{{ center[1] }},2) between POWER({{ (height)-(6*stripe_height) }},2) and POWER({{ (height)-(5*stripe_height) }},2) then '#4815AA' -- purple
+    else colour
 end
 {% endmacro %}
