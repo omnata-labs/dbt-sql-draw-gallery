ad1bcaeff76cb4163b56687415ffcd297c385087
James Weakley <jameswillisweakley@gmail.com>
2021-08-15 21:18:43 +1000

    Revert "Rescale oval macro"
    This reverts commit 8d6d29e9e162f60ecc979e6bf5da4c05ac019e5c.

--- macros/shapes/is_in_oval.sql
+++ macros/shapes/is_in_oval.sql
@@ -1,5 +1,4 @@
 {% macro is_in_oval(center=[100,100], diameter=15, ratio_x=0.5,ratio_y=1, rotation=0) %}
-power(cast((x-{{ center[0] }})*cos({{ rotation }})-(y-{{ center[1] }})*sin({{ rotation }}) as int)*{{ratio_y}},2) + 
-power(cast((x-{{ center[0] }})*sin({{ rotation }})+(y-{{ center[1] }})*cos({{ rotation }}) as int)*{{ratio_x}},2) < power({{ diameter }},2)
+POWER(cast( (x-{{ center[0] }})*cos({{ rotation }})-(y-{{ center[1] }})*sin({{ rotation }}) as int)*{{ratio_y}},2) + POWER(cast((x-{{ center[0] }})*sin({{ rotation }})+(y-{{ center[1] }})*cos({{ rotation }}) as int)*{{ ratio_x }},2) < {{ diameter }}
 
 {% endmacro %}
