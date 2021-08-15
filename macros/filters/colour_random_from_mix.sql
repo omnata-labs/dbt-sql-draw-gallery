922fc24847e9291f311670e9caed0c37a21546d4
James Weakley <jameswillisweakley@gmail.com>
2021-08-15 20:13:20 +1000

    Fixes

--- macros/filters/colour_random_from_mix.sql
+++ macros/filters/colour_random_from_mix.sql
@@ -1,13 +1,13 @@
 {% macro colour_random_from_mix(colour_mix=[['red',0.2],['green',0.2],['blue',0.6]]) %}
 {% set current_point = namespace(value=0) %}
-select coalesce(max(column3),'{{ colour_mix[0][0] }}') from (values
+(select coalesce(max(column3),'{{ colour_mix[0][0] }}') from (values
   {% for colour in colour_mix %}
   ({{ current_point.value }}, {{ current_point.value + colour[1]}}, '{{ colour[0]}}')
   {% if not loop.last %}, {% endif %}
   {% set current_point.value = current_point.value + colour[1] %}
   {% endfor %}
   ) v
-where x-x + random() between column1 and column2
+where x-x + random() between column1 and column2)
 {% endmacro %}
 
 
--- macros/objects/colour_tessellating_rectangle.sql
+++ macros/objects/colour_tessellating_rectangle.sql
@@ -1,4 +1,7 @@
 {% macro colour_tessellating_rectangle(width=10,height=20,odd_colour='#ffffff',even_colour='#000000') %}
-when mod(x/{{ width }},2) != mod(y/{{ height }},2) then '{{ odd_colour }}'
-when mod(x/{{ width }},2) = mod(y/{{ height }},2) then '{{ even_colour }}'
+case
+    when mod(x/{{ width }},2) != mod(y/{{ height }},2) then '{{ odd_colour }}'
+    when mod(x/{{ width }},2) = mod(y/{{ height }},2) then '{{ even_colour }}'
+    else colour -- shouldn't ever match
+end
 {% endmacro %}
