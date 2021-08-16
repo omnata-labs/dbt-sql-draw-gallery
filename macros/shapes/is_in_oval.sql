5e42392124ac15a5b57fc91843d3e1c8aeb46d05
James Weakley <jameswillisweakley@gmail.com>
2021-08-16 11:14:54 +1000

    Improve spiral diameter consistency

--- macros/shapes/is_in_oval.sql
+++ macros/shapes/is_in_oval.sql
@@ -1,4 +1,9 @@
 {% macro is_in_oval(center=[100,100], diameter=15, ratio_x=0.5,ratio_y=1, rotation=0) %}
-POWER(cast( (x-{{ center[0] }})*cos({{ rotation }})-(y-{{ center[1] }})*sin({{ rotation }}) as int)*{{ratio_y}},2) + POWER(cast((x-{{ center[0] }})*sin({{ rotation }})+(y-{{ center[1] }})*cos({{ rotation }}) as int)*{{ ratio_x }},2) < {{ diameter }}
+{% set min_ratio = [ratio_x, ratio_y] | min %}
+{% set min_ratio_inverted = 1 / min_ratio %}
+
+power(cast((x-{{ center[0] }})*cos({{ rotation }})-(y-{{ center[1] }})*sin({{ rotation }}) as int)*{{ ratio_y }},2) + 
+power(cast((x-{{ center[0] }})*sin({{ rotation }})+(y-{{ center[1] }})*cos({{ rotation }}) as int)*{{ ratio_x }},2) 
+< power({{ diameter / min_ratio_inverted / 2 }},2)
 
 {% endmacro %}
