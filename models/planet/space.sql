ca80b20b0046a859af2c4b6d3d8d41d6258e03e4
James Weakley <jameswillisweakley@gmail.com>
2021-08-16 18:21:12 +1000

    Add stars to space scene

--- models/planet/space.sql
+++ models/planet/space.sql
@@ -1,5 +1,9 @@
 {{ config(materialized='new_layer') }}
-
-select x,y, '#000000' as colour
+select x,y, 
+case
+    -- 1% chance of a pixel becoming a distant star
+    when x-x+random() < 0.01 then '#ffffff'
+    else '#000000'
+end as colour
 from {{ ref('blank_canvas_1000_1000') }}
 
