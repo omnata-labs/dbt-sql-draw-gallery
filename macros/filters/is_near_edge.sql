7790291a31d399553a2cd1b2b8fc7b39932d3979
James Weakley <jameswillisweakley@gmail.com>
2021-09-23 16:30:29 +1000

    Fix density range in is_near_edge macro

--- macros/filters/is_near_edge.sql
+++ macros/filters/is_near_edge.sql
@@ -1,6 +1,6 @@
 {% macro is_near_edge(distance=1,density_at_distance=[1]) %}
     {% set density_distances = density_at_distance | length %}
-    {% for i in range(1,distance) %}
+    {% for i in range(1,distance+1) %}
     (
         {% if i <= density_distances and density_at_distance[i-1] < 1 %}
             x-x+random()< {{ density_at_distance[i-1] }} and
